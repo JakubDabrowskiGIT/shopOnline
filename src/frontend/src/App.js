fbdbd579d041c27750b9eff5c3e394e143139816
Jakub D <crimson.kuba@gmail.com>
2021-03-20 00:49:27 +0100

    first changes is App.js

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,23 +1,27 @@
+import React, {useState, useEffect} from "react";
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+
+const Item = () => {
+
+  const fetchItems = () => {
+    axios.get("http://localhost:8080/backend").then(res => {
+      console.log(res);
+    });
+  }
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  return "Hello"
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Item />
     </div>
   );
 }
