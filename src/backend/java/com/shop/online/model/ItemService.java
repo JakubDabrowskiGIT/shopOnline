e8a2272738e7bb8974c9938c5f3dcd25259dc621
Jakub D <crimson.kuba@gmail.com>
2021-03-22 01:17:18 +0100

    add h2db as sql db to project

--- src/backend/java/com/shop/online/model/ItemService.java
+++ src/backend/java/com/shop/online/model/ItemService.java
@@ -3,9 +3,9 @@ package com.shop.online.model;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -30,7 +30,7 @@ public class ItemService {
     }
 
     public Set<Item> getAll() {
-        return itemRepository.findAll().stream().collect(Collectors.toSet());
+        return new HashSet<>(itemRepository.findAll());
     }
 
     public void save(List<Item> itemList) {
